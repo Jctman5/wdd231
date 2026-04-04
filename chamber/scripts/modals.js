9962f06a4378b7ab43ac2541fe19252046831edf
Jctman5 <Jctman5@gmail.com>
2026-04-04 14:43:40 -0400

    week4 upd

--- chamber/scripts/modals.js
+++ chamber/scripts/modals.js
@@ -4,3 +4,8 @@ document.querySelectorAll("[data-modal]").forEach(link => {
     document.getElementById(link.dataset.modal).showModal();
   });
 });
+document.querySelectorAll(".close-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    btn.parentElement.close();
+  });
+});
